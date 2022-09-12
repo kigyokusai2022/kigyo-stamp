df5582a969649b0d7f651bc9499f9e2e7ceaef52
Propromp <p@propromp.net>
2022-09-12 18:11:45 +0900

    fix: ui変更

--- src/App.svelte
+++ src/App.svelte
@@ -6,61 +6,88 @@
 
 <style>
 	.background {
+		position: absolute;
+		top: 0;
+
 		height: 100vh;
 		width: 100vw;
-
-		position: absolute;
 
 		background-image: url("../img/background.jpg");
 		background-size: cover;
 		background-position: center;
 	}
-	.stamps {
+
+	.background-white-effect {
 		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translateX(-50%) translateY(-50%);
+		top: 0;
 
-		padding: 20px;
-		margin: 0;
+		height: 100vh;
+		width: 100vw;
 
+		background-color: white;
+		opacity: 0.2;
+	}
+
+	.loading {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.box {
+		position: absolute;
+
+		box-sizing: border-box;
+
+		height: 100vh;
+		width: 100vw;
+
+		padding: 30px;
+
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.stamps {
 		display: grid;
 		align-items: center;
 		justify-content: center;
-
-		background-color: white;
-
-		border: solid yellow 20px;
-
-		filter: drop-shadow(0 0 10px gray);
 	}
 
-	@media screen and (max-aspect-ratio: 1/1) {
+	@media screen and (max-aspect-ratio: 2/3) {
 		.stamps {
 			height: auto;
-			width: min(60vw,50vh);
+			width: 60vw;
 			grid-template-rows: 1fr 1fr 1fr;
 			grid-template-columns: 1fr 1fr;
 		}
 	}
 
-	@media screen and (min-aspect-ratio: 1/1) {
+	@media screen and (min-aspect-ratio: 2/3) {
 		.stamps {
 			height: auto;
-			width: auto;
+			width: 50vw;
 			grid-template-rows: 1fr 1fr;
 			grid-template-columns: 1fr 1fr 1fr;
 		}
 	}
 </style>
+<div class="background"></div>
+<div class="background-white-effect"></div>
 {#if $authenticated}
-	<div class="background"></div>
-	<div class="stamps">
-		<Logo/>
-		{#each $stamps as stamp}
-			<StampBox stampId={stamp.id} stampName={stamp.name}/>
-		{/each}
+	<div class="box">
+		<p>各参加団体にあるQRコードをカメラアプリで読み込んでスタンプを集めよう！<br>スタンプをすべて回収したら、一号館1Fエントランスの受付で、景品と交換しよう！</p>
+		<div class="stamps">
+			<Logo/>
+			{#each $stamps as stamp}
+				<StampBox stampId={stamp.id} stampName={stamp.name}/>
+			{/each}
+		</div>
 	</div>
 {:else}
-	<p>読み込み中...</p>
+	<div class="loading">
+		<h1>読み込み中...</h1>
+	</div>
 {/if}
--- src/components/StampBox.svelte
+++ src/components/StampBox.svelte
@@ -57,7 +57,7 @@
     }
 
     p {
-        color: gray;
+        color: #333;
     }
 
     @keyframes stamp-claim {
